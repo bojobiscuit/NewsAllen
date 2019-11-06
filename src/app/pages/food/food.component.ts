dbe4e62b7e6aa574b9514f0c794c7656a59ef102
bojobiscuit <46871428+bojobiscuit@users.noreply.github.com>
2019-11-06 16:04:59 -0500

    Initial stylings. removed jquery and bootstrap

--- src/app/pages/food/food.component.ts
+++ src/app/pages/food/food.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
-  styleUrls: ['./food.component.sass']
+  styleUrls: ['./food.component.scss']
 })
 export class FoodComponent implements OnInit {
 
